fc2f50cb07673683d6b5ceffb2f2c3eb90b4db9a
Павел Вавилов <pavelvavilov@Pavels-MacBook-Pro-2.local>
2025-06-23 22:19:23 +0300

    Add new method to Calculations

--- plot_results.py
+++ plot_results.py
@@ -17,9 +17,9 @@ class PrepareData():
         ''' 
         Returns data from JSON file, sorted with NNs layers
         Params:
-        barcode_json_path - path to file with data
+            barcode_json_path - path to file with data
         Return:
-        layer1, ..., layerN - N lists with data for each layer
+            layer1, ..., layerN - N lists with data for each layer
         '''
         # Read data from JSON file
         with open(barcode_json_path, 'r') as data:
@@ -40,10 +40,10 @@ class PrepareData():
         Call read_barcode_data func and put it in one dict with amount of data
         as a key and a list of data as a value
         Params:
-        amounts - list with data amount numbers
-        base_path - common path to all files with data 
+            amounts - list with data amount numbers;
+            base_path - common path to all files with data 
         Return:
-        Dictionary with data amounts as keys and the data as values 
+            Dictionary with data amounts as keys and the data as values 
         '''
         # Collecting data into the dictionary per amount of data
         barcode_data = {}
@@ -67,9 +67,9 @@ class Calculations():
         ''' 
         Calculates the homogies mean lfietimes for one layer
         Params:
-        layer - data for one layer
+            layer - data for one layer.
         Return:
-        Mean lifetime value
+            Mean lifetime value
         '''
         last_epoch = layer[-1]['H0']
         lifetime = 0
@@ -82,13 +82,39 @@ class Calculations():
 
         return mean_lifetime
 
+    def calculate_homology_mean_lifetime_per_epochs(self, data_amount: list) -> dict:
+        ''' 
+        Calculates the homogies mean lfietimes for each epoch per one amount of data
+        Params:
+            data_amount - data for one data amount.
+        Return:
+            Dict with mean lifetime value per epochs
+        '''
+        total_lifetime = dict()
+        epoch_num = 1
+
+        for epoch in data_amount:
+            curr_epoch = epoch['H0']
+            lifetime = 0
+
+            for i in range(len(curr_epoch)):
+                mean = curr_epoch[i][1] - curr_epoch[i][0]
+                lifetime += mean 
+
+            mean_lifetime_part = lifetime / len(data_amount)
+
+            total_lifetime[epoch_num] = mean_lifetime_part
+            epoch_num += 1
+
+        return total_lifetime
+
     def calculate_persist_entropy(self, layer: list) -> float:
         ''' 
         Calculates entropy for each layer in NNs
         Params:
-        layer - data for one layer
+            layer - data for one layer
         Return:
-        Value of an entropy for with layer
+            Value of an entropy for with layer
         '''
         last_epoch = layer[-1]['H0']
         pair_difference = list()
@@ -113,9 +139,9 @@ class Calculations():
         ''' 
         Calculates normalized persistent entropy from basic entropy
         Params:
-        layers_ent - list with entropy values for each layer and each data amount
+            layer - list with entropy values for each layer and each data amount
         Return:
-        Normilized entropy for specific layer and amount of data
+            Normilized entropy for specific layer and amount of data
         '''
         last_epoch = layer[-1]['H0']
         pair_difference = list()
@@ -136,6 +162,40 @@ class Calculations():
 
         return -h / log2(len(pair_difference))
     
+    def calculate_norm_persist_entropy_per_epochs(self, data_amount: list) -> dict:
+        ''' 
+        Calculates normalized persistent entropy from basic entropy for each epoch per one data amount.
+        Params:
+            data_amount - list with entropy values for each epoch for one data amount.
+        Return:
+            Normilized entropy for specific data amount and each epoch.
+        '''
+        total_entropy = dict()
+        epoch_num = 1
+
+        for epoch in data_amount:
+            curr_epoch = epoch['H0']
+            pair_difference = list()
+
+            for pair in curr_epoch:
+                pair_difference.append(pair[1] - pair[0])
+            
+            result = list()
+            for diff_pair in pair_difference:
+                result.append(diff_pair / sum(pair_difference))
+
+            h = 0
+            for elem in result:
+                if elem == 0:
+                    pass
+                else:
+                    h =+ elem * log2(elem)
+                    
+            total_entropy[f'{epoch_num}'] = -h / log2(len(pair_difference))
+            epoch_num += 1
+
+        return total_entropy
+    
 
 class PlotResults():
     '''  
@@ -148,11 +208,11 @@ class PlotResults():
         ''' 
         Build the graphs for data amounts and mean lifetime values
         Params:
-        mean_lifetime_value - list of mean lifetime values
-        data_amounts - list of data amounts for this experiment
-        layer_name - name of a NNs hidden layer the graph belongs to
+            mean_lifetime_value - list of mean lifetime values;
+            data_amounts - list of data amounts for this experiment;
+            layer_name - name of a NNs hidden layer the graph belongs to
         Return:
-        Graph for data amounts and mean lifetime values 
+            Graph for data amounts and mean lifetime values 
         '''
         plt.plot(data_amounts, mean_lifetime_value, marker='o')
         plt.title(layer_name)
@@ -166,10 +226,10 @@ class PlotResults():
         ''' 
         Plots a grid of graphs for mean lifetime through the data amounts for each layer
         Params:
-        data_amounts - list with data amounts
-        layers - list with lists with layer data
+            data_amounts - list with data amounts;
+            layers - list with lists with layer data
         Return:
-        None
+            None
         '''
         # Create figure and grid with X rows and 1 column with common X axis
         fig, axes = plt.subplots(layers_num, 1, figsize=(10, 8), sharex=True)
@@ -194,10 +254,10 @@ class PlotResults():
         ''' 
         Plots graph for start mean lifetime points per each layer 
         Params:
-        layers - list with mean lifetimes for each layer;
-        data_amount - 0-0, 1-1000, 2-2000, 3-3000, 4-4000, 5-5000, 6-10000, 7-20000, 8-30000, 9-40000, 10-50000, 11-60000.
+            layers - list with mean lifetimes for each layer;
+            data_amount - 0-0, 1-1000, 2-2000, 3-3000, 4-4000, 5-5000, 6-10000, 7-20000, 8-30000, 9-40000, 10-50000, 11-60000.
         Return:
-        None
+            None
         '''
         l = [i for i in range(1, layers_num+1)]
         first_point = [lay[data_amount_index] for lay in layers]
@@ -214,11 +274,11 @@ class PlotResults():
         ''' 
         Build the graphs for data amounts and entropy values
         Params:
-        ent_value - list of entropy values
-        data_amounts - list of data amounts for this experiment
-        layer_name - name of a NNs hidden layer the graph belongs to
+            ent_value - list of entropy values;
+            data_amounts - list of data amounts for this experiment;
+            layer_name - name of a NNs hidden layer the graph belongs to
         Return:
-        Graph for data amounts and entropy values 
+            Graph for data amounts and entropy values 
         '''
         plt.plot(data_amounts, ent_value, marker='o')
         plt.title(layer_name)
@@ -233,13 +293,14 @@ def save_model_metrics_to_json(json_path: str, model_name: str, accuracy: float,
     ''' 
     This metod saves obtained models metrics into the JSON file
     Params:
-    json_path - path to JSON file to store the data;
-    model_name - models name as a key from saving data;
-    accuracy - models classification metric;
-    precision - models classification metric;
-    recall - models classification metric;
-    f1_score - models classification metric.
-    Output: None
+        json_path - path to JSON file to store the data;
+        model_name - models name as a key from saving data;
+        accuracy - models classification metric;
+        precision - models classification metric;
+        recall - models classification metric;
+        f1_score - models classification metric.
+    Output: 
+        None
     '''
     # Create a dict with model's metrics for saving im JSON file
     model_metrics = {
